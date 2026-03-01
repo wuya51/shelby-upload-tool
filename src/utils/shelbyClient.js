8cc229b7f442eda510cae2e155b0809c2bbea349
root <root@DESKTOP-T15A939.localdomain>
2026-03-01 11:12:45 +0800

    Add default moduleAddress to shelbyClient

--- src/utils/shelbyClient.js
+++ src/utils/shelbyClient.js
@@ -8,5 +8,5 @@ import { Network } from "@shelby-protocol/solana-kit/react";
 export const shelbyClient = new ShelbyClient({
   network: Network.SHELBYNET,
   apiKey: import.meta.env.VITE_SHELBY_API_KEY || "",
-  moduleAddress: import.meta.env.VITE_SHELBY_MODULE_ADDRESS,
+  moduleAddress: import.meta.env.VITE_SHELBY_MODULE_ADDRESS || "0x85fdb9a176ab8ef1d9d9c1b60d60b3924f0800ac1de1cc2085fb0b8bb4988e6a",
 });
